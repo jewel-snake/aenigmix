6e2b82ee69fa6f7898964d08016388d15278d2d2
jewel-snake <a.usatyuk03@proton.me>
2022-09-04 10:34:52 +0300

    code update for multiple time usage of single key

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,31 @@
-pub mod cipher;
+use std::io::{Write,Read};
+
+mod kolakoski;
+
+pub use kolakoski::Kolakoski as Encoder;
+
+impl<T: Read+Write+std::io::Seek> Encoder<T> {
+  pub fn filter<M:Read,O:Write>(&mut self, target: &mut M,output: &mut O) {
+    let bytes = target.bytes();
+    for byte in bytes {
+      output.write(&[byte.unwrap() ^ self.get_byte()]).unwrap();
+    }
+  }
+}
+/*
+pub fn chiper_file_with_cert<T:Read,U:Read>(target: &mut T, cert: &mut U) {
+  let mut output = std::fs::OpenOptions::new().write(true).create(true).read(false).open("ciphered_output.bin").unwrap();
+  filter(target, cert, &mut output);
+}
+pub fn decipher_file_with_cert<T:Read,U:Read>(target: &mut T, cert: &mut U) {
+  let mut output = std::fs::OpenOptions::new().write(true).create(true).read(false).open("deciphered_output.txt").unwrap();
+  filter(target, cert, &mut output);
+}*/
+pub fn filter<T:Read,U:Read,O:Write>(target: &mut T, cert: &mut U, output: &mut O) {
+  let mut file = std::fs::OpenOptions::new().read(true).write(true).create(true).open("kolakoski.bin").unwrap();
+  let mut kolakoski = kolakoski::Kolakoski::new(cert,&mut file);
+  let bytes = target.bytes();
+  for byte in bytes {
+    output.write(&[byte.unwrap() ^ kolakoski.get_byte()]).unwrap();
+  }
+}
--- tests/check_cipher.rs
+++ tests/check_cipher.rs
@@ -1,13 +1,16 @@
 use std::fs::{File, OpenOptions};
 use std::io::{Read,Write};
-use aenigmix::cipher;
+use aenigmix::Encoder;
 #[test]
 fn check_cipher() {
   initialize_files();
   cipher_file();
   let mut cert = File::open("cert.crt").unwrap();
   let mut ciphered = File::open("ciphered_output.bin").unwrap();
-  cipher::decipher_file_with_cert(&mut ciphered,&mut cert);
+  let mut output = std::fs::OpenOptions::new().write(true).create(true).read(false).open("deciphered_output.txt").unwrap();
+  let mut inner = std::fs::OpenOptions::new().read(true).write(true).create(true).open("kolakoski.bin").unwrap();
+  let mut kolakoski = Encoder::new(&mut cert,&mut inner);
+  kolakoski.filter(&mut ciphered,&mut output);
   let deciphered = File::open("deciphered_output.txt").unwrap();
   let input = File::open("hello_world.txt").unwrap();
   for (a, b) in deciphered.bytes().zip(input.bytes()) {
@@ -18,7 +21,11 @@ fn check_cipher() {
 fn cipher_file() {
   let mut  cert = File::open("cert.crt").unwrap();
   let mut  file = File::open("hello_world.txt").unwrap();
-  cipher::chiper_file_with_cert(&mut file,&mut cert);
+  let mut output = std::fs::OpenOptions::new().write(true).create(true).read(false).open("ciphered_output.bin").unwrap();
+  //filter(&mut file, &mut cert, &mut output);
+  let mut inner = std::fs::OpenOptions::new().read(true).write(true).create(true).open("kolakoski.bin").unwrap();
+  let mut kolakoski = Encoder::new(&mut cert,&mut inner);
+  kolakoski.filter(&mut file,&mut output);
 }
 
 fn initialize_files() {
